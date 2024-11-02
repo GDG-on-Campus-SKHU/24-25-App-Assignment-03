d38cc87725420f4822f107e160f60b32205d4387
hyeminililo <ng040115@gmail.com>
2024-11-02 11:44:54 +0900

    과제 내용 수정
    기존 과제에서 난이도를 조금 낮춰, 진행하기 위한 과제 내용 수정 및 ReadMe 수정

--- hw_01/book.dart
+++ hw_01/book.dart
@@ -8,4 +8,11 @@ class Book {
   Book(this.title, this.author);
 
   Future<void> borrow() async {}
+
+  Future<void> returnBook() async {
+    print('반납을 시도 중입니다: $title');
+    await Future.delayed(Duration(seconds: 1));
+    isAvailable = true;
+    print('$title이 반납되었습니다.');
+  }
 }
